06a8737baa8e46da5f76d468b3e6bc16637fd362
MuhaddisRehman <96332700+MuhaddisRehman@users.noreply.github.com>
2023-11-21 22:39:40 +0500

    initialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'punjab-university-affiliated-colleges-study-material';
+  databaseResources = [
+    {
+      title:
+        'Testbank for Database Systems Design, Implementation, & Management',
+      href: '/Assets/Database Systems/- Testbank for Database Systems Design, Implementation, & Management.pdf',
+    },
+    // ... other items
+  ];
+  getStarted() {
+    // Implement your logic here
+  }
 }
